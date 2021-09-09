38ed3ad78e454fa244c8b5f52e1b5c712769d59d
Faizan Zahid <faizan.zahid2500@gmail.com>
2021-09-09 17:30:33 +0500

    Add footer and remove comments

--- src/__mocks__/mockCounter.js
+++ src/__mocks__/mockCounter.js
@@ -1,7 +1,6 @@
 let count = 0;
 
 const counterDummy = (arr) => {
-  // menuTarget.textContent = data !== null ? `${mealCategory} (${data.length})` : 0;
   if (arr.length) {
     count = arr.length;
     return count;
